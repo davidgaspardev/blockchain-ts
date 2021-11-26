fb30de9a15f027414d6727af6d2f4a2cfb044327
David Gaspar <davidgaspar.dev@gmail.com>
2021-11-26 15:38:59 -0300

    json2hex and string2hex functions added - close #3

--- src/core/utils/format.ts
+++ src/core/utils/format.ts
@@ -1,3 +1,11 @@
 export function getTimestamp(): number {
     return Number.parseInt(new Date().getTime().toString().slice(0, -3));
 }
+
+export function json2hex(data: Object): string {
+    return Buffer.from(JSON.stringify(data)).toString('hex');
+}
+
+export function string2hex(data: string): string {
+    return Buffer.from(data).toString('hex');
+}
