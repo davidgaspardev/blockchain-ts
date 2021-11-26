60e032f865980567055aa73edc4b60c0c50d5292
David Gaspar <davidgaspar.dev@gmail.com>
2021-11-26 15:13:05 -0300

    Hashs setted test updated

--- src/core/block.spec.ts
+++ src/core/block.spec.ts
@@ -19,9 +19,14 @@ describe("Block tests", () => {
     });
     
     test("Setter hashs (SHA256)", () => {
-        block.setHash("92fac3bd7c7be7a654d45de39d3f9da0bc3aa7f64c904252ef439436f3c4c2a0");
-        block.setPreviousHash("b662f16d7a12165ba1f6e0004a415d93ef74c8eebf0ad6ab7c10ed495d54001d");
+        // Dummy hashes
+        const hash = "92fac3bd7c7be7a654d45de39d3f9da0bc3aa7f64c904252ef439436f3c4c2a0";
+        const previousHash = "b662f16d7a12165ba1f6e0004a415d93ef74c8eebf0ad6ab7c10ed495d54001d";
 
-        expect(block.getPreviousHash().length).toEqual(64);
+        block.setHash(hash);
+        block.setPreviousHash(previousHash);
+
+        expect(block.getHash()).toEqual(hash);
+        expect(block.getPreviousHash()).toEqual(previousHash);
     });
 });
